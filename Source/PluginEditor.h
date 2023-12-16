1721ed2f5fbd5ed28dd61e554faa600897f9b02a
Adolf <owner@Users-MacBook-Air.local>
2023-12-16 10:39:42 -0800

    initial commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class GainLevelAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                       public juce::Slider::Listener
+{
+public:
+    GainLevelAudioProcessorEditor (GainLevelAudioProcessor&);
+    ~GainLevelAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    void sliderValueChanged(juce::Slider* slider) override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    GainLevelAudioProcessor& audioProcessor;
+    
+    juce::Slider gainSlider;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainLevelAudioProcessorEditor)
+};
